339dd1d63d38de59435c97126524cf028f4be3c3
Rajeswari0407 <78481252+Rajeswari0407@users.noreply.github.com>
2021-03-04 17:06:01 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:Abcde@12345@localhost/test"
 # get environment variable
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
